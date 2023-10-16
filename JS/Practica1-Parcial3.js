d7ce82d8b927f0da03960532d696f6cb3c97d536
ChristopherOrtiz69 <christopher.villafanet@gmail.com>
2023-10-16 13:13:16 -0600

    subiendo practicas

---
+++ JS/Practica1-Parcial3.js
@@ -0,0 +1,30 @@
+let $h1 = document.querySelector("h1");
+$h1.innerText = "Christopher Ivan Ortiz Vilafañet";
+$h1.style.fontFamily= "Courier";
+$h1.style.color = "Yellow";
+$h1.style.backgroundColor = "Orange";
+
+let $img = document.querySelector("img");
+$img.src = "https://cdn.discordapp.com/attachments/899797214757543978/1154271370193150014/foto.png?ex=6539e97b&is=6527747b&hm=8c844a6e1cba3831d25cedc4c818f6627c0bb04e1a0dd6b1d81958dbc28a0003&";
+$img.style.borderRadius="50%";
+$img.style.width= "450px";
+$img.style.height = "550px";
+
+let $p = document.querySelector("p");
+$p.innerText = " Naci el 6 de agosto del 2000, soy aficionado de el Automovilismo, principalmente de la Formula 1, me causa mucho placer poder crear videojuegos en VR ya que la interaccion me gusta mucho, actualmente estoy estudiando Ing en desarrollo de Software Interactivo y Videojuegos en la CDMX.";
+$p.style.fontFamily= "Verdana";
+$p.style.color = "Purple";
+$p.style.backgroundColor = "Green";
+
+let $ol = document.querySelector("ol");
+$ol.innerHTML = "<li>Practicar BOX</li><li>Probar juegos aunque no los termine</li><li>Testear experiencias VR </li><li>Aventarme de cosas con altura</li><li>Dormir </li>";
+$ol.style.fontFamily= "Arial";
+$ol.style.color = "Purple";
+$ol.style.backgroundColor = "Black";
+
+let $ul = document.querySelector("ul");
+$ul.innerHTML = "<li>Chris-ivan06@hotmail.com</li><li>Telefono: 5526839879</li>";
+$ul.style.fontFamily= "Arial";
+$ul.style.color = "Yellow";
+$ul.style.backgroundColor = "Green";
+
